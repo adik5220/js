18787773450914710460026bd0119b169dbbc81e
adik5220 <adik5220@mail.ru>
2022-01-24 18:17:03 +0600

    First commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use sctrict";
+// const result = confirm('Are u here?');
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(typeof(answer));
+
+const amswers = [];
+
+amswers[0] = prompt('Как Ваше имя?', '');
+amswers[1] = prompt('Как Ваше фамилия?', '');
+amswers[2] = prompt('Как Ваше лет?', '');
+
+document.write(amswers);
