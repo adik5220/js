7087e59dc887f45906ef2d5703b94d124c70d0d4
adik5220 <adik5220@mail.ru>
2022-01-24 21:48:07 +0600

    Условия и циклы

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ let numberOfFilms;
 
 numberOfFilms = +prompt('Сколь фильмов Вы уже посмотрели?', '');
 
+
+
 let personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -12,12 +14,35 @@ let personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+if (personalMovieDB.count < 10) {
+    alert('Посмотрено довольно мало фильмов');
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    alert('Вы классный зритель!');
+} else if (personalMovieDB.count >30) {
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка');
+}
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+for (let i=0; i<2; i++){
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done!');
+    } else {
+        console.log('Error!');
+        i--;
+    }
+
+}
 
 console.log(personalMovieDB);
